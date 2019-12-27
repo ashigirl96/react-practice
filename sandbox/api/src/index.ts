2fac0089775cc6c56a2a840678e91804d6456907
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2019-12-27 11:57:10 +0900

    Deploy先でfunctionとhostingが動くことを確認した

---
+++ sandbox/api/src/index.ts
@@ -0,0 +1,9 @@
+import * as functions from "firebase-functions";
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+export const helloWorld = functions.https.onRequest((request: functions.Request, response: functions.Response) => {
+ response.send("Hello from Firebase!");
+});
+console.log("loaded helloworld");
